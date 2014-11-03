ca948d4b57ce59070c12b4543485b64e62f23bf2
YangMann <zhangyang93@rocketmail.com>
2014-11-03 15:33:26 +0800

    java web setup using gumby

---
+++ basic/src/CheckVersion.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+/**
+ *
+ * Created by Yang ZHANG on 2014/10/28.
+ */
+public class CheckVersion {
+
+    private final static String URL = "jdbc:postgresql://localhost:5432/YAGO";
+    private final static String USER = "postgres";
+    private final static String PASSWORD = "19930311";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT VERSION()");
+
+            if (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
